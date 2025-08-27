fa2f35aeab1973a41be5582dfbbf8db2762ccc11
John Opiyo <john.opiyo_oneacrefu@KE60A5E2084F21.KE>
2025-08-27 12:10:33 +0300

    Basic Calculator

---
+++ Calculater.py
@@ -0,0 +1,30 @@
+#1. ask the user to use the first number
+num1 = float(input("Enter yur first number: "))
+
+#2. ask the user to input the second number
+num2 = float(input("Enter you second number: "))
+
+#3. ask the user to input an operational symbol
+operation = input("Enter and operator(+,-,*,/,%): ")
+
+#4. use if, elif and else to perform the operation
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1}+{num2}={result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1}-{num2}={result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1}*{num2}={result}")
+elif operation == "%":
+    result = num1 % num2
+    print(f"{num1}%{num2}={result}")
+elif operation == "/":
+    if num2 !=0:
+        result = num1 / num2
+        print(f"{num1}/{num2}={result}")
+    else:
+        print("Error: Cannot be divedied by zero")
+else:
+ print("Invalid operation please use (+,-,/,*,%)")
